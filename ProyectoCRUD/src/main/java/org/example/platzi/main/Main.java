0713e101352750a27ddaba612cf585291970983d
danielsperezb <danielestebanperez22@gmail.com>
2023-09-24 16:42:06 -0500

    JAVA SWING muestra App Java con mejor diseño del funcionamiento

--- ProyectoCRUD/src/main/java/org/example/platzi/main/Main.java
+++ ProyectoCRUD/src/main/java/org/example/platzi/main/Main.java
@@ -4,53 +4,14 @@ import org.example.platzi.model.Employee;
 import org.example.platzi.repository.EmployeeRepository;
 import org.example.platzi.repository.Repository;
 import org.example.platzi.util.DatabaseConnection;
+import org.example.platzi.view.SwingApp;
 
 import java.sql.*;
 
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-
-        try (Connection myConn = DatabaseConnection.getInstance()) {
-            Repository<Employee> repository = new EmployeeRepository();
-
-            System.out.println("-----LISTANDO----");
-            repository.findAll().forEach(System.out::println);
-
-            //System.out.println("---BUSCANDO POR ID----");
-            //System.out.println(repository.getById(100));
-
-
-            //System.out.println("----INSERTANDO UN EMPLEADO----");
-            //Employee employee = new Employee();
-            //employee.setFirst_name("Diego");
-            //employee.setPa_surname("Alejandro ");
-            //employee.setMa_surname("Martinez");
-            //employee.setEmail("diego@gmail.com");
-            //employee.setSalary((float)2000);
-            //repository.save(employee);
-
-
-
-            //System.out.println("----ACTUALIZANDO UN EMPLEADO, YA EXISTENTE CON ID----");
-            //Employee employee = new Employee();
-            //employee.setId(6);
-            //employee.setFirst_name("Diego");
-            //employee.setPa_surname("Esteban");
-            //employee.setMa_surname("Martinez");
-            //employee.setEmail("diego@gmail.com");
-            //employee.setSalary((float)2000);
-            //repository.save(employee);
-
-
-            System.out.println("ELIMINAR UN REGISTRO");
-            repository.delete(6);
-
-            System.out.println("--VERIFIACION--");
-            repository.findAll().forEach(System.out::println);
-
-
-
-        }
+        SwingApp app =new SwingApp();
+        app.setVisible(true);
     }
 }
