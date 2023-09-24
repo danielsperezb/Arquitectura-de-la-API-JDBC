e2cb2402c94fe29f83bbe955fc1ea399fe24fb79
danielsperezb <danielestebanperez22@gmail.com>
2023-09-24 14:08:06 -0500

    IMPLEMENTACION PATRON REPOSITORY

--- ProyectoCRUD/src/main/java/org/example/platzi/main/Main.java
+++ ProyectoCRUD/src/main/java/org/example/platzi/main/Main.java
@@ -1,20 +1,18 @@
 package org.example.platzi.main;
 
+import org.example.platzi.util.DatabaseConnection;
+
 import java.sql.*;
 
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/project";
-        String user = "root";
-        String password = "";
 
-        try (Connection myConn = DriverManager.getConnection(url, user, password)){
+        try (Connection myConn = DatabaseConnection.getInstance()){
             Statement myStamt =  myConn.createStatement();
             ResultSet myRes = myStamt.executeQuery("SELECT * FROM employees ORDER BY pa_surname");
 
-            while (myRes.next()) {
-                System.out.println(myRes.getString("first_name") + "," + myRes.getString("email"));
+            while (myRes.next()) {                System.out.println(myRes.getString("first_name") + "," + myRes.getString("email"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
